c9fe99bbbd1abc2e93f67bc326713e2f07ebce85
Laura <garch.laura@gmail.com>
2024-02-13 23:04:30 +0100

    cp07

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -5,13 +5,12 @@
 
 #include <iostream>
 
-template < typename T >
-void	iter( T *arr, unsigned int n, void ( *f )( T& ) )
+template <typename T> void iter(T *arr, unsigned int n, void (*f)(T&))
 {
-	if ( arr != NULL )
+	if (arr != NULL)
 	{
-		for ( unsigned int i = 0; i < n; i++ )
-			f( arr[ i ] );
+		for (unsigned int i = 0; i < n; i++)
+			f(arr[i]);
 	}
 }
 
