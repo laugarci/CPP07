02ddf28b5b7175bc00aa63c25a13eccb573c1d21
Laura Garcia Arch <garch.laura@gmail.com>
2024-02-18 20:19:39 +0100

    cpp07

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -1,4 +1,14 @@
-
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: laugarci <laugarci@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/18 19:53:09 by laugarci          #+#    #+#             */
+/*   Updated: 2024/02/18 20:19:28 by laugarci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef ITER_HPP
 #define ITER_HPP
@@ -12,6 +22,11 @@ template <typename T> void iter(T *arr, unsigned int n, void (*f)(T&))
 		for (unsigned int i = 0; i < n; i++)
 			f(arr[i]);
 	}
+}
+
+template <typename T> void print(T &x)
+{
+    std::cout << x << std::endl;
 }
 
 #endif
