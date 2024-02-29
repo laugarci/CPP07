dc77e32b44f736411be9ce602473481608468f56
Laura Garcia Arch <garch.laura@gmail.com>
2024-02-29 11:54:42 +0100

    cpp07

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: laugarci <laugarci@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/18 20:07:22 by laugarci          #+#    #+#             */
-/*   Updated: 2024/02/28 12:03:53 by laugarci         ###   ########.fr       */
+/*   Updated: 2024/02/29 11:54:26 by laugarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,10 @@
 
 int main( void )
 {
-	int a = 0;
-	int b = 23;
-	
+	int a = 2;
+	int b = 3;
+
+	std::cout << BGRED "[ SUBJECT TEST ]" RESET << std::endl;
 	::swap( a, b );
 	std::cout << "a = " << a << ", b = " << b << std::endl;
 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
@@ -27,5 +28,31 @@ int main( void )
 	std::cout << "c = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+	std::cout << BGRED "[ MORE TESTS ]" RESET << std::endl;
+	std::cout << std::endl << BGRED "[ num test ]" RESET << std::endl;
+	int e = 42;
+	int f = 84;
+	std::cout << GREEN << "Before:  " RESET << "c = " << e << " d = " << f << std::endl;
+	::swap(e, f);
+	std::cout << GREEN << "After: " RESET << " c = " << e << " d = " << f << std::endl;
+	std::cout << GREEN "Min num: " RESET << ::min(e, f) << std::endl;
+	std::cout << GREEN "Max num: " RESET << ::max(e, f) << std::endl;
+	e = 10;
+	f = -5;
+	std::cout << GREEN << "Before:  " RESET << "c = " << e << " d = " << f << std::endl;
+	::swap(e, f);
+	std::cout << GREEN << "After: " RESET << " c = " << e << " d = " << f << std::endl;
+	std::cout << GREEN "Min num: " RESET << ::min(e, f) << std::endl;
+	std::cout << GREEN "Max num: " RESET << ::max(e, f) << std::endl;
+
+	std::cout << std::endl << BGRED "[ string test ]" RESET << std::endl;
+	std::string hello = "hello";
+	std::string bye = "bye";
+	std::cout << GREEN "Min string: " RESET << ::min(hello, bye) << std::endl;
+	std::cout << GREEN "Max string: " RESET << ::max(hello, bye) << std::endl;
+	std::cout << GREEN << "Before:  " RESET << "hello = " << hello << " / bye = " << bye << std::endl;
+	::swap(hello, bye);
+	std::cout << GREEN << "After: " RESET << " hello = " << hello << " / bye = " << bye << std::endl;
 	return 0;
 }
--- ex00/whatever.hpp
+++ ex00/whatever.hpp
@@ -6,12 +6,18 @@
 /*   By: laugarci <laugarci@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/18 20:07:14 by laugarci          #+#    #+#             */
-/*   Updated: 2024/02/18 20:07:20 by laugarci         ###   ########.fr       */
+/*   Updated: 2024/02/29 11:37:29 by laugarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WHATEVER_HPP
 #define WHATEVER_HPP
+
+#define BOLD "\x1B[1m"
+#define RESET "\x1B[0m"
+#define RED "\x1B[31m"
+#define BGRED "\x1B[41m"
+#define GREEN "\x1B[32m"
 
 #include <iostream>
 
